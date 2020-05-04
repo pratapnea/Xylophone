26ca1351cf428ec8f92324b95ef34afc158f080a
Pratap Neaupane <sereversion@gmail.com>
2020-05-04 14:58:43 +0545

    Code Wrapped in functions

--- lib/main.dart
+++ lib/main.dart
@@ -4,92 +4,42 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+//  method to play the sound
+  void ring(int colorNumber) {
+    final player = AudioCache();
+    player.play('note$colorNumber.wav');
+  }
+
+//  return the Expanded widget
+  Expanded buildKey({Color color, int keyNumber}) {
+    return Expanded(
+      child: FlatButton(
+        onPressed: () {
+          ring(keyNumber);
+        },
+        color: color,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Center(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      final player = AudioCache();
-                      player.play('note1.wav');
-                    },
-                    child: Container(
-                      color: Colors.blue,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      final player = AudioCache();
-                      player.play('note2.wav');
-                    },
-                    child: Container(
-                      color: Colors.red,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      final player = AudioCache();
-                      player.play('note3.wav');
-                    },
-                    child: Container(
-                      color: Colors.green,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      final player = AudioCache();
-                      player.play('note4.wav');
-                    },
-                    child: Container(
-                      color: Colors.yellow,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      final player = AudioCache();
-                      player.play('note5.wav');
-                    },
-                    child: Container(
-                      color: Colors.cyan,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      final player = AudioCache();
-                      player.play('note6.wav');
-                    },
-                    child: Container(
-                      color: Colors.lightGreen,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: FlatButton(
-                    onPressed: () {
-                      final player = AudioCache();
-                      player.play('note7.wav');
-                    },
-                    child: Container(
-                      color: Colors.teal,
-                    ),
-                  ),
-                ),
+                buildKey(color: Colors.blue, keyNumber: 1),
+                buildKey(color: Colors.red, keyNumber: 2),
+                buildKey(color: Colors.green, keyNumber: 3),
+                buildKey(color: Colors.yellow, keyNumber: 4),
+                buildKey(color: Colors.lightBlue, keyNumber: 5),
+                buildKey(color: Colors.lightGreen, keyNumber: 6),
+                buildKey(color: Colors.teal, keyNumber: 7),
               ],
             ),
           ),
